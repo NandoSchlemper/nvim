83b061123af32d63cf5c95491ff67957d6a4f927
NandoSchlemper <bkschlemper@gmail.com>
2025-02-11 10:05:03 -0300

    Feat: ToggleTerm C-\

--- lua/beka/lazy_init.lua
+++ lua/beka/lazy_init.lua
@@ -15,3 +15,17 @@ require("lazy").setup({
   spec = "beka.lazy",
   change_detection = {notify = false}
 })
+
+local err, tterm = pcall(require, "toggleterm")
+if not err then
+    return
+end
+vim.cmd [[let &shell = '"C:\Program Files\Git\bin\bash.exe"']]
+vim.cmd [[let &shellcmdflag = '-s']]
+tterm.setup({
+    open_mapping = '<C-\\>',
+    start_in_insert = true,
+    direction = 'float',
+    size = 10
+})
+
