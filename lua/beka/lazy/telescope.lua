2dc12539216d4143c4068b91c34aec6fa37d9893
NandoSchlemper <bkschlemper@gmail.com>
2025-02-22 16:19:35 -0300

    New features

--- lua/beka/lazy/telescope.lua
+++ lua/beka/lazy/telescope.lua
@@ -5,12 +5,12 @@ return {
 
     dependencies = {
         "nvim-lua/plenary.nvim"
-    }, 
+    },
     config = function()
         require('telescope').setup({})
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>bf', builtin.buffers, {})
+        vim.keymap.set('n', '<C-a>', builtin.find_files, {})
+        vim.keymap.set('n', '<C-z>', builtin.buffers, {})
     end
 }
 
